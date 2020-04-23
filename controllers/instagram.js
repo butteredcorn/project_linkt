5c6d39936ca56455252f6ae5e709b18f7f33b854
justin <justin_yih@hotmail.com>
2020-04-22 17:09:41 -0700

    setup instagram

---
+++ controllers/instagram.js
@@ -0,0 +1,44 @@
+const axios = require('axios')
+const { URLSearchParams } = require('url')
+
+const getInstagramAuthWindow = (redirectURI) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const instagramID = process.env.INSTAGRAM_APP_ID
+            const instagramAuthWindow = `https://api.instagram.com/oauth/authorize`+
+            `?client_id=${instagramID}`+
+            `&redirect_uri=${redirectURI}`+
+            `&scope=user_profile,user_media`+
+            `&response_type=code`
+            resolve(instagramAuthWindow)
+        } catch (error) {
+            console.log(error)
+            reject(error)
+        }
+    })
+}
+
+const getInstagramAccessToken = (redirectURI, instagramCode) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const instagramAppID = process.env.INSTAGRAM_APP_ID
+            const instagramAppSecret = process.env.INSTAGRAM_APP_SECRET
+            const params = new URLSearchParams()
+            params.append('client_id', instagramAppID)
+            params.append('client_secret', instagramAppSecret)
+            params.append('grant_type', 'authorization_code')
+            params.append('redirect_uri', redirectURI)
+            params.append('code', instagramCode)
+            return axios({
+                method: 'POST',
+                url: 'https://instagram.com/oauth/access_token',
+                data: params
+            })
+        }
+    })
+}
+
+module.exports = {
+    getInstagramAuthWindow,
+    getInstagramAccessToken
+}
---
+++ routes/instagram-endpoint.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const router = express.Router()
+const { protectedRoute } = require('../controllers/authentication')
+const { getInstagramAuthWindow, getInstagramAccessToken } = require('../controllers/instagram')
+
+router.get('/login', protectedRoute, async (req, res) => {
+    try {
+        const redirectURI = `https://${req.get('host')}/instagram/returnURL`
+        const instagramAuthWindow = await getInstagramAuthWindow(redirectURI)
+        res.redirect(instagramAuthWindow)
+    } catch (error) {
+        console.log(error)
+        res.send('Error with instagram login.')
+    }
+})
+
+router.get('/returnURL', protectedRoute, async (req, res) => {
+    try {
+        const redirectURL = `https://${req.get('host')}/instagram/returnURL`
+        const instagramCode = req.query.code
+        await getInstagramAccessToken(redirectURL, instagramCode)
+        .then((user) => {
+            user.email = req.user.email
+            console.log(user)
+            res.send('complete.')
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.send('Error with instagram login.')
+    }
+})
