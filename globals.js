211ff72e407ea3ac60e032fd16d93b9220eb5e4b
justin <justin_yih@hotmail.com>
2020-04-24 18:31:19 -0700

    update globals

--- globals.js
+++ globals.js
@@ -1,6 +1,8 @@
 module.exports = {
     admins: [ process.env.DATABASE_ADMIN ],
     bcryptSaltRounds: parseInt(process.env.SALT_ROUNDS),
+
+    NUM_IG_PHOTOS_PUSHED_TO_DB: 10,
     maximumInstagramPhotosForProcessing: 5,
 
     defaultMaxDistanceKMs: 25,
